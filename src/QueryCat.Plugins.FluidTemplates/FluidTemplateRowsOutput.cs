84f594c4ec3e4856b0e776b2e64cd7194d999f5c
Ivan Kozhin <ivan@saritasa.com>
2024-01-20 13:52:32 +0700

    Update backend, bump versions

--- src/QueryCat.Plugins.FluidTemplates/FluidTemplateRowsOutput.cs
+++ src/QueryCat.Plugins.FluidTemplates/FluidTemplateRowsOutput.cs
@@ -5,6 +5,7 @@ using Fluid.Ast;
 using Fluid.Values;
 using QueryCat.Backend.Core;
 using QueryCat.Backend.Core.Data;
+using QueryCat.Backend.Core.Execution;
 using QueryCat.Backend.Core.Functions;
 using QueryCat.Backend.Core.Types;
 using QueryCat.Backend.Execution;
